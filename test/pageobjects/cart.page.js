0891492c63ccb742fedfbb857ef3e51494696a7f
Martina Sierra <martinasierra94@gmail.com>
2021-05-28 12:08:26 -0300

    Add tests

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -1,14 +1,15 @@
 import Page from './page';
 
 class CartPage extends Page {
-  
+
     //Buttons
     get btnCheckOut() { return $('[name="checkout"]')}
     get btnContinueShopping() { return $('[name="continue-shopping"]')}
-   
-    //Back to
     get btnB2products() { return $('[name="back-to-products"]')}
     get btnB2shopping() { return $('[name="continue-shopping"]')}
+
+    //
+    get rmCartItems() { return $$('.removed_cart_item')}
     
     open () {
         return super.open('cart.html');
--- test/specs/cartTest.js
+++ test/specs/cartTest.js
@@ -4,30 +4,54 @@ import CartPage from '../pageobjects/cart.page';
 
 describe('Shopping cart', () => {
 
-    it('should select a product and proceed to checkout',() => {
+    it('should add a product to cart, visualize it in the cart and proceed to checkout',() => {
         LoginPage.open();
         LoginPage.login('standard_user','secret_sauce');
         browser.pause(2000);
         InventoryPage.btnAddTestTShirt.click();
         InventoryPage.btnCart.click();
-        browser.pause(2000);
+        expect(browser).toHaveUrl('https://www.saucedemo.com/cart.html');
+        expect(InventoryPage.titleLinkSelector(3)).toBeDisplayed();
         CartPage.btnCheckOut.click()
         browser.pause(2000);
         expect(browser).toHaveUrl('https://www.saucedemo.com/checkout-step-one.html');
     }); 
 
-    it('should return to the Inventory from the Cart by clicking in Continue Shopping', () => {
+    it('should add all the products to cart, visualize them in the cart and proceed to checkout',() => {
         InventoryPage.open();
+        InventoryPage.btnAddBikeLight.click();
+        InventoryPage.btnAddBackpack.click();
+        InventoryPage.btnAddBoltTShirt.click();
         InventoryPage.btnAddFleeceJacket.click();
-        browser.pause(2000);
+        InventoryPage.btnAddOnesie.click();   
         InventoryPage.btnCart.click();
+        expect(browser).toHaveUrl('https://www.saucedemo.com/cart.html');         
+        for (let index = 0; index < 5; index++) {
+            expect(InventoryPage.titleLinkSelector(index)).toBeDisplayed();
+        }
+        CartPage.btnCheckOut.click()
         browser.pause(2000);
-        CartPage.btnContinueShopping.click();
+        expect(browser).toHaveUrl('https://www.saucedemo.com/checkout-step-one.html');
+    }); 
+
+    it('should remove all products from the cart', () => {
+        CartPage.open();
+        InventoryPage.btnRmBikeLight.click();
+        InventoryPage.btnRmBackpack.click();
+        InventoryPage.btnRmBoltTShirt.click();
+        InventoryPage.btnRmFleeceJacket.click();
+        InventoryPage.btnRmTestTShirt.click();
+        InventoryPage.btnRmOnesie.click();
         browser.pause(2000);
-        expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+        expect(CartPage.rmCartItems.length).toEqual(6);
     });
 
-    it('should return to the Inventory from the Cart by clicking in All Items', () => {
+    it('should return to the Inventory by clicking in Continue Shopping', () => {
+    CartPage.btnContinueShopping.click();
+    expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+    });
+
+    it('should return to the Inventory by clicking in All Items', () => {
         InventoryPage.btnCart.click();
         InventoryPage.btnMenu.click();
         InventoryPage.btnAllItems.click();
